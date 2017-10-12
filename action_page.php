7fdb4ca2273ddad6bea7677b1c50b7cc9dec5b14
Luciano <luchososa1990@hotmail.com.ar>
2017-10-12 16:40:27 -0300

    correo

---
+++ action_page.php
@@ -0,0 +1,36 @@
+<?php
+  $persona = $_POST["Nombre"];
+  $correo = $_POST["Email"];
+  $consulta = $_POST["Mensaje"];
+
+  $destinatario = "contactothinco@gmail.com";
+  $asunto = "Consulta sobre piletas";
+  $cuerpo = '
+  <html>
+  <head>
+     <title>Consulta</title>
+  </head>
+  <body>
+  <h1>'.$persona.'</h1>
+  <p>
+  Responder a: '.$correo.'
+  </p>
+  <p>
+  '.$consulta.'
+  </p>
+  </body>
+  </html>
+  ';
+
+  //para el envío en formato HTML
+  $headers = "MIME-Version: 1.0\r\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+
+  //dirección del remitente
+  $headers .= "From: ".$persona." < ".$correo." >\r\n";
+
+
+  mail($destinatario,$asunto,$cuerpo,$headers);
+  header('Location: contactos.php');
+
+?>
